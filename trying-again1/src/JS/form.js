3f4d7a6039b9808521a5cdb711b7b9f8b4d78476
Liet10 <alberto.aless2016@gmail.com>
2023-06-03 13:07:56 +0100

    03/06 arreglar Home

--- trying-again1/src/JS/form.js
+++ trying-again1/src/JS/form.js
@@ -1,18 +1,34 @@
 export default function validateForm() {
     const form = document.getElementById("form_contacto");
-    const button = document.getElementById("button_sumbmit");
 
-    validatePhone();
+    onlyNumber();   
 
-    button.addEventListener('click', () => {        
-            form.setAttribute('action', "https://formsubmit.co/algonsopj1962@gmail.com");
-            form.setAttribute('method', "POST");
-            form.submit();
-        
-    });
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); // Evita que el formulario se envíe
+        const inputName = document.getElementById("name").value;
+        const inputMail = document.getElementById("email").value;
+        const inputPhone = document.getElementById("phone").value
+      
+        if (inputName === '' || inputMail === '') {  
+                const textError = 'Los campos nombre, correo son requeridos';
+                const errorElement = document.createElement('P');
+                errorElement.classList.add('error_element');
+                errorElement.textContent = textError;
+                form.appendChild(errorElement);
+                 setTimeout(()=>{
+                    errorElement.remove()
+                    form.reset();
+                 },5000)          
+        } else {
+              form.action = "https://formsubmit.co/alberto.aless2016@gmail.com";
+              form.method = "POST";
+              form.submit();
+
+        }
+      });
 }
 
-function validatePhone() {
+function onlyNumber() {
     const inputPhone = document.getElementById("phone");
 
     inputPhone.addEventListener("keydown", function(event) {
@@ -20,7 +36,7 @@ function validatePhone() {
         if ((event.key < "0" || event.key > "9") && event.key !== " " && event.key !== "Backspace") {
             event.preventDefault();
         }
-    });
+    });  
 
 }
 
