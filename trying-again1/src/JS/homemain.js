8a76a619317638828c4826e3f685bc5de3e167fd
Liet10 <alberto.aless2016@gmail.com>
2023-06-07 17:41:51 +0100

    last

--- trying-again1/src/JS/homemain.js
+++ trying-again1/src/JS/homemain.js
@@ -1,12 +1,7 @@
 export default function MainContainer() {
-<<<<<<< HEAD
-  const isHomePage = document.body.classList.contains('home-page');
   
-=======
-  const currentPath = window.location.pathname;
-  const isHomePage = currentPath === '/index.html'; // Actualiza esto con la ruta de tu página de inicio
+  const isHomePage = document.body.classList.contains('home-page'); // Actualiza esto con la ruta de tu página de inicio
 
->>>>>>> 3034f5cd1c80d7c4183516f46e36610ec0cdf04f
   if (!isHomePage) {
     return; 
   }
